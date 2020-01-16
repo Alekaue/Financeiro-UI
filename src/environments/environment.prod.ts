595a0cffb77bb9f58a006bc34b1a6689eb826195
Alessandro Brito <alekaue.brito@gmail.com>
2020-01-15 22:52:54 -0300

    Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: ' https://financeiro-angular-alekaue.herokuapp.com',
+  apiUrl: ' https://financeiro-alekaue.herokuapp.com',
   whiteListedDomains: ['financeiro-alekaue.herokuapp.com'],
   blackListedRoutes: ['https://financeiro-alekaue.herokuapp.com/oauth/token']
 };
